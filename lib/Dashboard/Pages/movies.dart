4c18a29bd6518d6e09f7b67f3a3f599e05bec3cb
Narin <hongchhiev@gmail.com>
2022-08-12 13:00:27 +0700

    .

--- lib/Dashboard/Pages/movies.dart
+++ lib/Dashboard/Pages/movies.dart
@@ -18,6 +18,53 @@ class Movies extends StatelessWidget {
               color: Color.fromARGB(255, 139, 154, 186),
               fontWeight: FontWeight.w500,
               fontSize: 21)),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "Discover",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                child: Column(
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                              child: Image.asset(
+                                "lib/assets/movie/movie1.jpg",
+                                height: 70,
+                                width: 90,
+                              ),
+                            )
+                          ],
+                        ))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
